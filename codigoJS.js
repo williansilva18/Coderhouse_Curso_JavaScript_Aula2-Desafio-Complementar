22c183f939fbb9b3fc12fb75df17ca433ee244c6
Willian Silva <willian.silva.1992@hotmail.com>
2023-08-17 22:56:23 -0300

    Subindo o desafio complementar

---
+++ codigoJS.js
@@ -0,0 +1,16 @@
+var nome = prompt("Digite o seu nome:");
+console.log("Olá, " + nome + "!");
+
+var salvarNumero = 2;
+var numeroDigitado = prompt("Digite um número:");
+
+var resultado = salvarNumero * parseInt(numeroDigitado);
+console.log("O número digitado foi: " + numeroDigitado);
+console.log("O resultado da multiplicação * 2 é: " + resultado);
+
+var nomeConcatenar = prompt("Digite o primeiro nome:");
+var sobrenomeConcatenar = prompt("Digite o seu sobrenome:");
+var textoConcatenado = nomeConcatenar + " " + sobrenomeConcatenar;
+
+alert("O texto concatenado é: " + textoConcatenado);
+
